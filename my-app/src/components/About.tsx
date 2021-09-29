6d9d84047b411067d4e0d7a3b8a94ed788f6d21d
mhvue <mhvue4@gmail.com>
2021-09-29 13:23:57 -0500

    added spacing for header and padding for images and about info section to start.

--- my-app/src/components/About.tsx
+++ my-app/src/components/About.tsx
@@ -6,6 +6,7 @@ import Grid from '@mui/material/Grid';
 import varietyCupcakes from "../varietyCupcakes.jpg"
 import NavBar from './Navbar';
 
+
 const Item = styled(Paper)(({ theme }) => ({
     ...theme.typography.body2,
     padding: theme.spacing(1),
@@ -14,21 +15,20 @@ const Item = styled(Paper)(({ theme }) => ({
   }));
 
 
-
 const About = () => {
     return (
       <>
-      <h1>Cupcakes</h1>
+      <h1 style={{textAlign: "center"}}>Cupcakes</h1>
       <NavBar />
       <Box style={{marginTop: "40px", padding: "15px"}}sx={{ flexGrow: 1 }} >
         <Grid container spacing={2}>
           <Grid item xs={4}>
             <Item>Welcome!</Item>
-             <img alt="cupcakes" src={varietyCupcakes} width="90%"/>
+             <img style={{padding: "15px"}} alt="cupcakes" src={varietyCupcakes} width="90%"/>
           </Grid>
           <Grid item xs={8}>
             <Item>About</Item>
-            
+            <div style={{padding: "15px"}}>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
               scelerisque neque nunc, vitae malesuada nisi lobortis id. Fusce
               quis ipsum at turpis pretium sollicitudin. Donec tristique enim ac
@@ -36,7 +36,7 @@ const About = () => {
               vestibulum est tincidunt eu. Praesent bibendum at tortor quis
               pretium. Aliquam interdum ligula nisl. In ac sapien non ligula
               tincidunt lobortis et ut sapien.
-            
+            </div>
           </Grid>
         </Grid>
       </Box>
