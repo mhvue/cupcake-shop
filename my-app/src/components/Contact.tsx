8ce4c3845e8445021073baa343e6b49de38c15d6
mhvue <mhvue4@gmail.com>
2021-09-30 13:04:20 -0500

    added in draft version for Contact page.

--- my-app/src/components/Contact.tsx
+++ my-app/src/components/Contact.tsx
@@ -1,14 +1,50 @@
-import React from 'react';
-import NavBar from './Navbar';
+import React from "react";
+import NavBar from "./Navbar";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
 
 const Contact = () => {
-    return (
-        <div>
-            <h1 style={{textAlign: "center"}}>Contact</h1>
-            <NavBar />
-            Contact page.
+  return (
+    <div>
+      <h1 style={{ textAlign: "center" }}>Contact Us</h1>
+      <NavBar /> 
+      <p>Contact us with any questions.</p>
+      <Box component="form" sx={{display: "flex", flexWrap: "wrap"}}>
+        <div style={{ padding: "10px" }}>
+          <TextField
+            required
+            id="outlined-required"
+            label="First Name and Last Name"
+            defaultValue="Name"
+            sx={{ m: 1, width: '50ch' }}
+          />
+
+          <TextField
+            required
+            id="outlined-required"
+            label="Email"
+            defaultValue="email"
+            sx={{ m: 1, width: '50ch' }}
+          />
+
+          <TextField
+           label="Comment"
+           sx={{ m: 1, width: '100ch' }} 
+            required
+            multiline
+            rows={4}
+            id="Comment"
+            defaultValue="Comment"
+          />
+          
         </div>
-    )
+        <Button 
+            style={{ marginLeft: "15px"}}
+             variant="contained">Submit</Button>
+      </Box>
+    </div>
+  );
 };
 
 export default Contact;
